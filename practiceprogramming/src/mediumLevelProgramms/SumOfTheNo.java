eb8c034a9da9cdac271d4c65ba30bd5708eaa5ba
singhrina <rinasingh4@gmail.com>
2017-02-08 17:41:57 -0800

    new

---
+++ practiceprogramming/src/mediumLevelProgramms/SumOfTheNo.java
@@ -0,0 +1,21 @@
+package mediumLevelProgramms;
+
+import java.util.Scanner;
+
+public class SumOfTheNo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number :");
+		int num = sc.nextInt();
+		int sum = 0;
+
+		while (num > 0)   //123 >0
+		{
+			sum = sum + num % 10;  
+			num = num / 10;  
+			sc.close();
+		}
+		System.out.println(sum);
+	}
+
+}
