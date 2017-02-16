1971b0eebe3e19d121a74b264b2bebafe8fdcedc
singhrina <rinasingh4@gmail.com>
2017-02-15 17:19:26 -0800

    code test

---
+++ practiceprogramming/src/mediumLevelProgramms/PerfactNo.java
@@ -0,0 +1,31 @@
+package mediumLevelProgramms;
+
+import java.util.Scanner;
+
+public class PerfactNo {
+	public static void checkPefractNo() {
+		int n, sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the  no. to check :");
+		n = sc.nextInt();
+		sc.close();
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				sum = sum + i;
+			}
+		}
+		if (sum == n) {
+			System.out.println("given no is perfect ");
+		} else {
+			System.out.println("given no is not perfact");
+		}
+	}
+
+	int divisor(int x) {
+		return x;
+	}
+	public static void main(String[] args) {
+		PerfactNo.checkPefractNo();
+	}
+
+}
